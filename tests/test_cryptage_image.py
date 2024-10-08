179a09619b7a8848ec09a15e78842014588d7bb4
Mathieu Soussignan <mathieu@pc-98.home>
2024-10-08 09:48:38 +0200

    tests update

--- tests/test_cryptage_image.py
+++ tests/test_cryptage_image.py
@@ -9,31 +9,25 @@ from app.cryptage_image import charger_image, convertir_en_gris, crypter_image
 
 def test_charger_image():
     # Tester si l'image est correctement chargée
-    image = charger_image('../images/example.jpg')
-    print("Image chargée:", image.shape)  # Ajout d'un print pour afficher la forme de l'image
+    image = charger_image('images/example.jpg')
     assert image is not None
     assert len(image.shape) == 3  # Vérifier que l'image est bien en 3D (RGB)
 
 def test_conversion_en_gris():
     # Charger une image couleur
-    image = charger_image('../images/example.jpg')
-    print("Image couleur chargée:", image.shape)  # Ajout d'un print pour afficher la forme de l'image couleur
+    image = charger_image('images/example.jpg')
     
     # Convertir en niveaux de gris
-    image_gris = convertir_en_gris(image)
-    print("Image convertie en niveaux de gris:", image_gris.shape)  # Ajout d'un print pour afficher la forme de l'image en niveaux de gris
-    
+    image_gris = convertir_en_gris(image)    
     # Vérifier que l'image a bien été convertie en niveaux de gris
     assert len(image_gris.shape) == 2  # L'image en niveaux de gris doit être en 2D
 
 def test_crypter_image():
     # Charger une image couleur
-    image = charger_image('../images/example.jpg')
-    print("Image couleur chargée pour le cryptage:", image.shape)  # Ajout d'un print pour afficher la forme de l'image couleur
+    image = charger_image('images/example.jpg')
     
     # Crypter l'image
     image_cryptee = crypter_image(image)
-    print("Image cryptée:", image_cryptee.shape)  # Ajout d'un print pour afficher la forme de l'image cryptée
     
     # Vérifier que l'image cryptée a bien été transformée
     assert image_cryptee is not None
